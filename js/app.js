2540f2ef5c9d51dfe76001e20631640ab320959f
Sudhanshu shekhar <shekharsudhanshu801@gmail.com>
2024-02-03 16:35:29 +0530

    javascript

---
+++ js/app.js
@@ -0,0 +1,286 @@
+// // console.log("HELLO WORLD");
+// // console.log("Apna college");
+
+// // let a=123;
+// // let b=10;
+// // console.log("sum is : ",a+b);
+
+
+// let pencilPrice=10;
+// let penPrice=5;
+
+// //console.log(`the total price is ${pencilPrice+penPrice} rupees`);
+
+// //operators
+
+//  let a=10;
+//  let b=5;
+
+
+// // console.log(a++);
+// // console.log(++a);
+// // console.log(a+b);
+// // console.log(a-b);
+// // console.log(a*b);
+// // console.log(a/b);
+// // console.log(a%b);
+//  console.log(a**b);
+
+// // let color="yellow";
+
+// // if(color=="red")
+// // {
+// //     console.log("stop");
+// // }
+
+// // if(color=="yellow")
+// // {
+// //     console.log("slow down");
+// // }
+
+// // if(color=="green")
+// // {
+// //     console.log("gooo");
+// // }
+
+// // let age=14;
+// // if(age>=18){
+// //     console.log("you can vote");
+// // }
+// // else if(age<18){
+// //     console.log("you cannot vote");
+// // }
+// // else if(age<18)
+// // {
+// //     console.log("you cannot vote2")
+// // }
+
+// let size="XL";
+
+// if(size==="XL")
+// {
+//     console.log("price is Rs 250");
+// }
+// else if(size==="L")
+// {
+//     console.log("price is Rs 200");
+// }
+// else if(size==="M")
+// {
+//     console.log("price is Rs 100");
+// }
+// else if(size==="S")
+// {
+//     console.log("price is Rs 50");
+// }
+
+
+// //NESTED IF ELSE
+
+// let marks =75;
+
+// if(marks >=33)
+// {
+//     console.log("PASS");
+//     if(marks >=80)
+//     {
+//         console.log("Grade is: O");
+//     }
+//     else{
+//         console.log("Grade is: A");
+//     }
+
+// }
+// else{
+//     console.log("Better luck next time!!");
+// }
+
+
+
+// let str="app";
+// if(str[0]=='a' && str.length>3)
+// {
+//     console.log("Good string");
+// }
+// else{
+//     console.log("not a good string");
+// }
+
+// let num=12;
+
+// if((num%3===0) &&((num+1==15) || (num-1==11)))
+// {
+//     console.log("Safe");
+// }
+// else{
+//     console.log("unsafe");
+// }
+
+
+// if(1){
+//     console.log("it has true value");
+// }
+// else{
+//     console.log("it has  false value");
+// }
+
+// if(""){
+//     console.log("it has true value");
+// }
+// else{
+//     console.log("it has  false value");
+// }
+
+// if(" "){
+//     console.log("it has true value");
+// }
+// else{
+//     console.log("it has  false value");
+// }
+
+
+
+// let day=5;
+
+// switch(day){
+//     case 1:{
+//         console.log("Monday");
+//         break;
+//     }
+
+//     case 2:{
+//         console.log("Tuesday");
+//         break;
+//     }
+//     case 3:{
+//         console.log("Wednesday");
+//         break;
+//     }
+//     case 4:{
+//         console.log("Thursday");
+//         break;
+//     }
+
+//     case 5:{
+//         console.log("Friday");
+//         break;
+//     }
+//     case 6:{
+//         console.log("Saturday");
+//         break;
+//     }
+//     case 7:{
+//         console.log("Sunday");
+//         break;
+//     }
+//     default:{
+//         console.log("wrong input");
+//         break;
+//     }
+// }
+
+
+//  alert("error");
+
+//  console.log("this is simple log");
+//  console.error("this is error");
+//  console.warn("this is warn msg");
+
+//  let firstName=prompt("enter ur first name: ");
+//  let lastName=prompt("enter ur last name: ")
+//  console.log(firstName,lastName);
+
+
+// qs1
+
+// let num=20;
+
+// if(num%10===0)
+// {
+//     console.log("good");
+// }
+// else{
+//     console.log("bad");
+// }
+
+
+// // qs2
+
+// let name=prompt("Enter ur name: ")
+// let age=prompt("Enter ur age: ");
+
+// alert(`${name} is ${age} years old.`);
+
+// qs3
+
+// let quarter=1;
+
+// switch(quarter){
+//     case 1:{
+//         console.log("January,February,March");
+//         break;
+//     }
+//     case 2:{
+//         console.log("April,May,June");
+//         break;
+//     }
+//     case 3:{
+//         console.log("July,August,September");
+//         break;
+//     }
+//     case 4:{
+//         console.log("October,November,December");
+//         break;
+//     }
+//     default:{
+//         console.log("Invalid input");
+//         break;
+//     }
+// }
+
+
+//qs4
+
+//  let str="apple";
+//  if((str[0]==='A' || str[0]==='a') && (str.length>5)){
+//     console.log("Golden string");
+//  }
+//  else{
+//     console.log("Not a golden string");
+//  }
+
+
+//qs5
+
+// let a=10;
+// let b=15;
+// let c=20;
+
+// if(a>=b && a>=c)
+// {
+//     console.log(`${a} is the largest`);
+    
+// }
+// else if(b>=a && b>=c)
+// {
+//     console.log(`${b} is the largest`);
+    
+// }
+// else{
+//     console.log(`${c} is the largest`)
+// }
+
+
+// qs6
+
+let a=34;
+let b=47854;
+ let c=a%10;
+ let d=b%10;
+ if(c===d)
+ {
+    console.log(`${a} and ${b} has the same last digit`);
+ }
+ else{
+    console.log(`${a} and ${b} does not has the same last digit`);
+ }
---
+++ js/app3.js
@@ -0,0 +1,68 @@
+// let msg = "  he llo          ";
+
+// let password = prompt("set ur password");
+// let newPass=password.trim();
+// console.log(newPass);
+
+// let name="APNA COLLEGE";
+
+// console.log(name.toLowerCase());
+
+
+// let msg="ilovecoding";
+
+// let index=msg.indexOf("i");
+
+// console.log(index);
+
+
+// let msg="            hello    "
+
+// let newMsg=msg.trim().toUpperCase();
+// console.log(newMsg)
+
+
+// let msg="apnacollege";
+
+// console.log(msg.slice(4,11));
+
+// let msg="ILoveCoding";
+
+// console.log(msg.replace("Love","do"));
+
+// console.log(msg.repeat(3));
+
+// let msg="help!";
+
+// console.log(msg.trim().toUpperCase());
+
+// strings are immutable
+// arrays are mutable
+// let student1="aman";
+
+// let student2="shradha";
+
+// let student3="golu";
+
+// let students=["aman","shraddha","golu"];
+// console.log(students);
+
+// let info=["aman,90,8.5"];
+
+// let empArr=[];
+
+// let fruits=["mango", "apple", "litchi"];
+// fruits[0]="banana";
+
+// qs1
+// let array=[7,9,0,-2];
+// let n=3;
+// let ans=array.slice(0,n);
+// console.log(ans);
+
+// qs2
+let array=[7,9,0,-2];
+let n=1;
+let ans=array.splice(0,n);
+console.log(array);
+
